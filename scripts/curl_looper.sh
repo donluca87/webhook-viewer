a25a92e7aa6fe0e7799c19f319276a69468bcff2
Luca Donnaloia <luca.dnnl@gmail.com>
2023-10-03 00:09:46 +0200

    bash script added

---
+++ scripts/curl_looper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Loop 20 times
+for i in {1..20}; do
+  # Generate random data for login and password using openssl
+  login=$(openssl rand -hex 16)
+  password=$(openssl rand -hex 16)
+
+  # Make the curl request with random values and append a newline
+  curl -X POST http://localhost:3000/webhook -H 'Content-Type: application/json' -d "{\"login\":\"${login}\",\"password\":\"${password}\"}" >> output.txt &
+
+  # Optional: Sleep for a seconds between requests (adjust as needed)
+  sleep 1
+done
+
+# Wait for all background curl processes to finish
+wait
+
+# Add newlines between each output
+echo "" >> output.txt
